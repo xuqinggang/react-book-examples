0ad8953904a7c545696ba46b531cfd6a0691262e
qinggangxu <qingggangxu@sohu-inc.com>
2017-09-14 14:25:45 +0800

    test

--- 05/webpack.config.js
+++ 05/webpack.config.js
@@ -1,17 +1,18 @@
 var path = require('path');
 module.exports = {
-  entry: './src/app.js',
-  devtool: 'sourcemap',
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'app.bundle.js',
-    publicPath: '/build/'
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      include: path.join(__dirname, 'src'),
-      loader: 'babel'
-    }]
-  }
+    entry: './src/app.js',
+    devtool: 'sourcemap',
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'app.bundle.js',
+        publicPath: '/build/'
+    },
+    devtool: "module-eval-source-map",
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            include: path.join(__dirname, 'src'),
+            loader: 'babel'
+        }]
+    }
 };
